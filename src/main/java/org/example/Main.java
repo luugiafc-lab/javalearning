e808faa99038a9213378c8906b9a2f1482130e62
Lưu Gia Thành <ujuxl@student.kit.edu>
2026-02-02 22:15:36 +0100

    dauma

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("toi la thanh");
+        System.out.println("hay qua");
     }
 }
